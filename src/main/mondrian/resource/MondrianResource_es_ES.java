bf77f21dcbd099ddbd2a61cd7eab2999f65ecb2a
twixer <twixer@gmail.com>
2013-06-13 22:55:43 +0100

    add all jar files after a first ant compilation

---
+++ src/main/mondrian/resource/MondrianResource_es_ES.java
@@ -0,0 +1,24 @@
+// This class is generated. Do NOT modify it, or
+// add it to source control.
+
+package mondrian.resource;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import org.eigenbase.resgen.*;
+
+/**
+ * This class was generated
+ * by class org.eigenbase.resgen.ResourceGen
+ * from D:/workspace-juno/Mondrian_3.1.5/src/main/mondrian/resource/MondrianResource.xml
+ * on Thu Jun 13 23:42:34 CEST 2013.
+ * It contains a list of messages, and methods to
+ * retrieve and format those messages.
+ */
+
+public class MondrianResource_es_ES extends MondrianResource {
+    public MondrianResource_es_ES() throws IOException {
+    }
+}
+
+// End MondrianResource_es_ES.java
